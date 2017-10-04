69d1bd3fa19cb81d82349c42bfda481d51377aa7
warrencook6 <warren.cook6@gmail.com>
2017-10-03 23:00:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+// On email part
+$('.email').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.icon-paper-plane').addClass("next");
+    } else {
+      $('.icon-paper-plane').removeClass("next");
+    }
+  }
+);
+  //When you hover over next button turns cursor to pointer
+$('.next-button').hover(
+  function(){
+    $(this).css('cursor', 'pointer');
+  }
+);
+
+$('.next-button.email').click(
+  function(){
+    console.log("Something");
+    $('.email-section').addClass("fold-up");
+    $('.password-section').removeClass("folded");
+  }
+);
+
+// On PW Part
+$('.password').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.icon-lock').addClass("next");
+    } else {
+      $('.icon-lock').removeClass("next");
+    }
+  }
+);
+
+$('.next-button').hover(
+  function(){
+    $(this).css('cursor', 'pointer');
+  }
+);
+
+$('.next-button.password').click(
+  function(){
+    console.log("Something");
+    $('.password-section').addClass("fold-up");
+    $('.repeat-password-section').removeClass("folded");
+  }
+);
+
+// On PW confirm Part
+$('.repeat-password').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.icon-repeat-lock').addClass("next");
+    } else {
+      $('.icon-repeat-lock').removeClass("next");
+    }
+  }
+);
+
+$('.next-button.repeat-password').click(
+  function(){
+    console.log("Something");
+    $('.repeat-password-section').addClass("fold-up");
+    $('.success').css("marginTop", 0);
+  }
+);
